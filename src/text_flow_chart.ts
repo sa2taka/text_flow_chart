bf4947a5d4d9edd09765f034f2ee40ea1738a2e2
sa2taka <sa2taka@gmail.com>
2021-03-10 23:17:07 +0900

    :recycle: Fix some style

--- src/text_flow_chart.ts
+++ src/text_flow_chart.ts
@@ -2,7 +2,7 @@ import d3, { Graphviz } from 'd3-graphviz';
 import { createDotFromText } from './dot';
 
 export default {
-  render: (target: string | Graphviz<any, any, any, any>, text: String) => {
+  render(target: string | Graphviz<any, any, any, any>, text: String) {
     if (typeof target === 'string') {
       return d3.graphviz(`#${target}`).renderDot(createDotFromText(text));
     } else {
