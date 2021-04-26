4d896735273476a728f8796b9183fb47ed431b5a
sa2taka <sa2taka@gmail.com>
2021-04-26 10:07:55 +0900

    :wrench: fix types

--- src/text_flow_chart.ts
+++ src/text_flow_chart.ts
@@ -1,8 +1,20 @@
 import { Graphviz, graphviz } from 'd3-graphviz';
 import { createDotFromText } from './dot';
 
+// HACK: follow types are defined at @types/d3-graphviz
+interface GraphvizEnterElement {
+  ownerDocument: Document;
+  namespaceURI: string;
+  appendChild(newChild: Node): Node;
+  insertBefore(newChild: Node, refChild: Node): Node;
+  querySelector(selectors: string): Element;
+  querySelectorAll(selectors: string): NodeListOf<Element>;
+}
+
+type GraphvizBaseType = Element | GraphvizEnterElement | Document | Window | null;
+
 export default {
-  render(target: string | Graphviz<any, any, any, any>, text: String) {
+  render(target: string | Graphviz<GraphvizBaseType, any, GraphvizBaseType, any>, text: String) {
     if (typeof target === 'string') {
       return graphviz(`#${target}`).renderDot(createDotFromText(text));
     } else {
